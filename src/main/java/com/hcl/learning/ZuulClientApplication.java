dc0e84b179e0a08f0f4c712a949a815633b1436c
Mukul Kansal <kansalmukul@gmail.com>
2018-10-16 11:54:05 +0530

    Update ZuulClientApplication.java

--- src/main/java/com/hcl/learning/ZuulClientApplication.java
+++ src/main/java/com/hcl/learning/ZuulClientApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @EnableZuulProxy
